10e7a2de381c9c8914ca9cd69c176764d6ad8c04
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2023-05-22 22:13:30 +0300

    Add Computer Choice function

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,11 @@
+// Get Computer Choice function
+const getComputerChoice = function () {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    return "rock";
+  } else if (randomNumber === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
