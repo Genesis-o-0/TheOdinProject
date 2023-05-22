c3884dead8863c802df90af388a7905f7c0140d9
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2023-05-22 22:18:27 +0300

    Add playRound function

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -13,3 +13,37 @@ const getComputerChoice = function () {
 const getPlayerChoice = function () {
   return prompt('"rock", "paper", or "scissors"').toLowerCase();
 };
+// Player selection Declaration
+let playerSelection;
+// Computer selection Declaration
+let computerSelection;
+
+// Function for one Round
+function playRound(playerSelection, computerSelection) {
+  // Getting player selection
+  playerSelection = getPlayerChoice();
+  // Getting Computer selections
+  computerSelection = getComputerChoice();
+
+  if (computerSelection === playerSelection) {
+    return "It's a tie";
+  } else if (computerSelection === "rock") {
+    if (playerSelection === "paper") {
+      return "You Win! Paper beats Rock";
+    } else {
+      return "You Lose! Rock beats Scissors";
+    }
+  } else if (computerSelection === "paper") {
+    if (playerSelection === "scissors") {
+      return "You Win! Scissors beats Paper";
+    } else {
+      return "You Lose! Paper beats Rock";
+    }
+  } else if (computerSelection === "scissors") {
+    if (playerSelection === "paper") {
+      return "You Lose! Scissors beats Paper";
+    } else {
+      return "You Win! Rock beats Scissors";
+    }
+  }
+}
