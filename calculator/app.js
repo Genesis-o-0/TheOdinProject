d1e23ea8ee929f3e1ffadf762a97697b15165946
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2024-02-10 14:11:50 -0500

    Add delete functionality

--- calculator/app.js
+++ calculator/app.js
@@ -33,7 +33,7 @@ function subtract(num1, num2) {
 }
 
 function multiply(num1, num2) {
-  return Math.round((num1 * num2) * 100) / 100;
+  return Math.round(num1 * num2 * 100) / 100;
 }
 
 function divide(num1, num2) {
@@ -81,4 +81,10 @@ function getoperation(num1, operator, num2) {
     case "*":
       return multiply(num1, num2);
   }
+}
+
+function handleDelete() {
+  const resultArray = result.value.split("");
+  resultArray.splice(resultArray.length - 1, 1);
+  result.value = resultArray.join("");
 }
